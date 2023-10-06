7c4ef69d852f7311da37f6662ed341f875ac5495
Kyle-Atienza <atienzakylefrancis@gmail.com>
2023-10-06 23:41:09 +0800

    search by buyer functioanlity

--- src/pages/HistoryListPage.vue
+++ src/pages/HistoryListPage.vue
@@ -3,35 +3,119 @@
     <p v-if="!invoices.length && !$q.loading.isActive" class="text-h6 text-center">
       There are no transactions yet
     </p>
-    <history-item
-      v-for="(invoice, index) in invoices"
-      :key="index"
-      class="history-item"
-      :data="invoice"
-    />
+    <div class="row">
+      <outlined-text-input
+        class="col"
+        label="Buyer Name"
+        placeholder="Juan Dela Cruz"
+        v-model="buyer"
+      />
+      <q-btn
+        icon="search"
+        flat
+        unelevated
+        class="bg-secondary q-ml-md"
+        color="dark"
+        @click="onSearch"
+      />
+    </div>
+    <q-expansion-item
+      class="filters q-mt-sm"
+      header-class="filters__header"
+      icon="settings"
+      expand-icon-toggle
+    >
+      <template v-slot:header>
+        <q-item-section>
+          <div class="flex full-width">
+            <p class="text-h6 q-mb-none">Filters</p>
+            <div class="row q-gutter-sm q-ml-auto">
+              <q-btn
+                v-if="Object.keys(paramsObject).length > 1"
+                class="col bg-negative"
+                flat
+                color="secondary"
+                @click="onResetParams"
+                >Reset</q-btn
+              >
+              <q-btn class="col bg-secondary" flat color="dark" @click="fetchInvoices">Apply</q-btn>
+            </div>
+          </div>
+        </q-item-section>
+      </template>
+      <q-card>
+        <q-card-section class="filter-list q-pa-none">
+          <range-filter
+            ref="rangeItems"
+            group="history-filter"
+            label="No. of Items"
+            @update="onUpdateParams"
+          />
+          <range-filter
+            ref="rangeAmount"
+            group="history-filter"
+            label="Amount"
+            @update="onUpdateParams"
+          />
+          <range-filter
+            ref="rangeDate"
+            group="history-filter"
+            label="Date"
+            date
+            @update="onUpdateParams"
+          />
+        </q-card-section>
+      </q-card>
+    </q-expansion-item>
+    <div class="q-mt-md">
+      <history-item
+        v-for="(invoice, index) in invoices"
+        :key="index"
+        class="history-item"
+        :data="invoice"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import HistoryItem from 'src/components/cards/HistoryItem/HistoryItem.vue';
+import RangeFilter from 'src/components/common/SearchOptions/RangeFilter/RangeFilter.vue';
+import OutlinedTextInput from 'src/components/forms/input/OutlinedTextInput/OutlinedTextInput.vue';
+
 import { api } from 'src/boot/axios';
-import { onMounted, ref } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 import { useQuasar } from 'quasar';
 
 export default {
   components: {
     HistoryItem,
+    RangeFilter,
+    OutlinedTextInput,
   },
   setup() {
     const $q = useQuasar();
 
+    const rangeItems = ref(null);
+    const rangeAmount = ref(null);
+    const rangeDate = ref(null);
+
+    const buyer = ref('');
+
     const invoices = ref([]);
+    const paramsObject = ref({});
+    const requestParams = computed(() => {
+      const params = new URLSearchParams(paramsObject.value);
+      return params;
+    });
 
     const fetchInvoices = () => {
       $q.loading.show();
 
-      api
-        .get('/invoices')
+      api({
+        url: '/invoices',
+        params: requestParams.value,
+      })
         .then((res) => {
           const { data } = res.data;
 
@@ -43,13 +127,63 @@ export default {
         });
     };
 
+    const onUpdateParams = () => {
+      paramsObject.value = {
+        buyer: buyer.value,
+        total_min: rangeAmount.value.min || 0,
+        total_max: rangeAmount.value.max || 0,
+        items_min: rangeItems.value.min || 0,
+        items_max: rangeItems.value.max || 0,
+        date_min: rangeDate.value.dateRange.from
+          ? new Date(rangeDate.value.dateRange.from).toISOString()
+          : '',
+        date_max: rangeDate.value.dateRange.to
+          ? new Date(rangeDate.value.dateRange.to).toISOString()
+          : '',
+      };
+    };
+
+    const onSearch = () => {
+      onUpdateParams();
+      fetchInvoices();
+    };
+
+    const onResetParams = () => {
+      paramsObject.value = {
+        total_min: 0,
+        total_max: 0,
+        items_min: 0,
+        items_max: 0,
+        date_min: '',
+        date_max: '',
+      };
+
+      rangeItems.value.reset();
+      rangeAmount.value.reset();
+      rangeDate.value.reset();
+
+      fetchInvoices();
+    };
+
     onMounted(() => {
       fetchInvoices();
     });
 
     return {
-      $q,
       invoices,
+      rangeItems,
+      rangeAmount,
+      rangeDate,
+      paramsObject,
+      buyer,
+
+      requestParams,
+
+      onUpdateParams,
+
+      fetchInvoices,
+      onResetParams,
+      onSearch,
     };
   },
 };
@@ -61,4 +195,31 @@ export default {
     margin-top: 15px;
   }
 }
+
+.filters {
+  .q-card__section:first-child::after {
+    display: none;
+  }
+
+  ::v-deep &__header {
+    .q-icon {
+      color: $secondary;
+    }
+  }
+}
+
+.filter-list {
+  & > * {
+    &:not(:first-child) {
+      margin-top: 10px;
+    }
+  }
+}
+
+/* .filters {
+  .q-item-type {
+    min-height: 0;
+    padding: 8px 0;
+  }
+} */
 </style>
